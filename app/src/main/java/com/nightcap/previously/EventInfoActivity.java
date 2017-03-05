1fbe850692613d2e66d5faa1a5e82786d1ccac47
Paul Wong <paul.wong.88@gmail.com>
2017-03-05 18:00:57 +1100

    Add notes to event instance by clicking on the text to reveal a BottomSheet

--- app/src/main/java/com/nightcap/previously/EventInfoActivity.java
+++ app/src/main/java/com/nightcap/previously/EventInfoActivity.java
@@ -1,8 +1,15 @@
 package com.nightcap.previously;
 
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomSheetBehavior;
+import android.support.design.widget.BottomSheetDialogFragment;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.ActionBar;
@@ -11,9 +18,12 @@ import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import java.math.RoundingMode;
@@ -28,10 +38,14 @@ import java.util.List;
 
 public class EventInfoActivity extends AppCompatActivity implements ReceiveDateInterface, ReceiveEventInterface {
     private String TAG = "InfoActivity";
+
     private DatabaseHandler databaseHandler;
     private DateHandler dh = new DateHandler();
     private Event selectedEvent;
+
+    private FloatingActionButton fab;
     private TextView periodView, nextDueView, countView, intervalView;
+
     RecyclerView historyRecyclerView;
     private List<Event> historyList = new ArrayList<>();
     private HistoryAdapter historyAdapter;
@@ -41,11 +55,8 @@ public class EventInfoActivity extends AppCompatActivity implements ReceiveDateI
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_info);
 
-        // Get reference to ActionBar
-        ActionBar actionBar = getSupportActionBar();
-
         // Floating action button
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.event_info_fab);
+        fab = (FloatingActionButton) findViewById(R.id.event_info_fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -63,6 +74,9 @@ public class EventInfoActivity extends AppCompatActivity implements ReceiveDateI
         int eventId = getIntent().getIntExtra("event_id", 0);
         selectedEvent = databaseHandler.getEventById(eventId);
 
+        // Get reference to ActionBar
+        ActionBar actionBar = getSupportActionBar();
+
         // Configure ActionBar
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
@@ -79,7 +93,7 @@ public class EventInfoActivity extends AppCompatActivity implements ReceiveDateI
 
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
         historyRecyclerView.setLayoutManager(layoutManager);
-        historyRecyclerView.setItemAnimator(new DefaultItemAnimator());                // Animator
+        historyRecyclerView.setItemAnimator(new DefaultItemAnimator());         // Animator
         RecyclerView.ItemDecoration itemDecoration = new
                 DividerItemDecoration(this, DividerItemDecoration.VERTICAL);    // Decorator
         historyRecyclerView.addItemDecoration(itemDecoration);
@@ -100,9 +114,22 @@ public class EventInfoActivity extends AppCompatActivity implements ReceiveDateI
     }
 
     public void onReceiveEventFromAdapter(Event event, String flag) {
-        // Update info on cards to match the selected event from history
-        selectedEvent = event;
-        updateInfoCard();
+        if (flag.equalsIgnoreCase(HistoryAdapter.FLAG_ADD_NOTES)) {
+            // Hide the FAB
+            fab.hide();
+
+            // Show the soft keyboard
+//            InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+//            imm.showSoftInput(getCurrentFocus(), InputMethodManager.SHOW_FORCED);
+
+            // Show the BottomSheet
+            BottomSheetDialogFragment quickNotesFragment = new QuickNotesBottomSheetDialogFragment();
+            quickNotesFragment.show(getSupportFragmentManager(), quickNotesFragment.getTag());
+        } else if (flag.equalsIgnoreCase(HistoryAdapter.FLAG_EXPAND)) {
+            // Update info on cards to match the selected event from history
+            selectedEvent = event;
+            updateInfoCard();
+        }
     }
 
     private void updateInfoCard() {
@@ -168,11 +195,6 @@ public class EventInfoActivity extends AppCompatActivity implements ReceiveDateI
         databaseHandler.closeRealm();
     }
 
-    public void showDatePickerDialog(View v) {
-        DialogFragment newFragment = new DatePickerDialog();
-        newFragment.show(getSupportFragmentManager(), "datePickerDone");
-    }
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -199,4 +221,95 @@ public class EventInfoActivity extends AppCompatActivity implements ReceiveDateI
         }
         return super.onOptionsItemSelected(item);
     }
+
+    public void showDatePickerDialog(View v) {
+        DialogFragment newFragment = new DatePickerDialog();
+        newFragment.show(getSupportFragmentManager(), "datePickerDone");
+    }
+
+    // Bottom sheet for quickly entering event notes
+    public static class QuickNotesBottomSheetDialogFragment extends BottomSheetDialogFragment {
+        EditText quickNotes;
+        FloatingActionButton fab;
+
+        private BottomSheetBehavior.BottomSheetCallback bottomSheetBehaviorCallback
+                = new BottomSheetBehavior.BottomSheetCallback() {
+            @Override
+            public void onStateChanged(@NonNull View bottomSheet, int newState) {
+                // This is for interacting with the sheet directly
+                switch (newState) {
+                    case BottomSheetBehavior.STATE_DRAGGING:
+                        Log.i("BottomSheetCallback", "BottomSheetBehavior.STATE_DRAGGING");
+                        break;
+                    case BottomSheetBehavior.STATE_SETTLING:
+                        Log.i("BottomSheetCallback", "BottomSheetBehavior.STATE_SETTLING");
+                        break;
+                    case BottomSheetBehavior.STATE_EXPANDED:
+                        Log.i("BottomSheetCallback", "BottomSheetBehavior.STATE_EXPANDED");
+                        break;
+                    case BottomSheetBehavior.STATE_COLLAPSED:
+                        Log.i("BottomSheetCallback", "BottomSheetBehavior.STATE_COLLAPSED");
+                        break;
+                    case BottomSheetBehavior.STATE_HIDDEN:
+                        Log.i("BottomSheetCallback", "BottomSheetBehavior.STATE_HIDDEN");
+                        dismiss();
+                        break;
+                }
+            }
+
+            @Override
+            public void onSlide(@NonNull View bottomSheet, float slideOffset) {
+            }
+        };
+
+        @Override
+        public void setupDialog(Dialog dialog, int style) {
+            super.setupDialog(dialog, style);
+
+            // Inflate the view from XML
+            View bottomSheetView = View.inflate(getContext(), R.layout.bottom_sheet_quick_notes, null);
+            dialog.setContentView(bottomSheetView);
+
+            // Get and set Behavior params
+            CoordinatorLayout.LayoutParams params = (CoordinatorLayout.LayoutParams) ((View) bottomSheetView.getParent()).getLayoutParams();
+            CoordinatorLayout.Behavior behavior = params.getBehavior();
+
+            if(behavior != null && behavior instanceof BottomSheetBehavior) {
+                ((BottomSheetBehavior) behavior).setBottomSheetCallback(bottomSheetBehaviorCallback);
+            }
+
+            // Find the EditText box for text input
+            quickNotes = (EditText) bottomSheetView.findViewById(R.id.bottom_sheet_notes);
+            fab = (FloatingActionButton) getActivity().findViewById(R.id.event_info_fab);
+
+            // Detect when the dialog is actually shown on screen
+            dialog.setOnShowListener(new DialogInterface.OnShowListener() {
+                @Override
+                public void onShow(DialogInterface dialog) {
+                    // Show the soft keyboard
+                    InputMethodManager imm = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.showSoftInput(quickNotes, InputMethodManager.SHOW_IMPLICIT);
+
+                    // Set focus to the EditText
+                    quickNotes.requestFocus();
+                }
+            });
+        }
+
+        @Override
+        public void onDismiss(DialogInterface dialog) {
+            // Show the FAB when the BottomSheet is dismissed
+            if (!fab.isShown()) {
+                fab.show();
+            }
+        }
+
+//        @Override
+//        public void onCancel(DialogInterface dialog) {
+//            Log.i("onCancel", "FAB: " + f.isShown());
+//            f.show();
+//        }
+
+    }
+
 }
